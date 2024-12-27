8cff3b65df0ad17f1f1719af2faf4552dfaf2309
SivaGopal <157576579+Sivaatagile@users.noreply.github.com>
2024-12-27 10:43:00 +0530

    Report Process

---
+++ src/test/java/com/sendingReportAgilecyber/ReportProcess.java
@@ -0,0 +1,63 @@
+package com.sendingReportAgilecyber;
+
+import java.awt.AWTException;
+import java.io.IOException;
+
+import com.baseClass.Base;
+
+public class ReportProcess {
+
+	
+	public static void sendReport() throws InterruptedException, AWTException, IOException {
+		 
+//		 try {
+//			 
+//			 String folderPath = System.getProperty("user.dir");
+//			 System.out.println(folderPath);
+//	            Runtime.getRuntime().exec("explorer.exe " + folderPath);
+//	            Robot robot = new Robot();
+//
+//	            Thread.sleep(2000);
+//
+//	            robot.keyPress(KeyEvent.VK_F5);
+//	            robot.keyRelease(KeyEvent.VK_F5);
+//System.out.println("good ");
+//	        } catch (InterruptedException e) {
+//	            e.printStackTrace();
+//	        }
+//		 
+//		 
+//		 
+//		 
+//		  String projectPath = System.getProperty("user.dir") + "/target/cucumber-reports123/index.html";
+//	        System.out.println("HTML file path: " + projectPath);
+//
+//	        // Open the HTML file
+//	        try {
+//	            File htmlFile = new File(projectPath);
+//	            if (htmlFile.exists() && htmlFile.isFile()) {
+//	                // Open the HTML file using the default application
+//	                Desktop.getDesktop().open(htmlFile);
+//	                System.out.println("HTML file opened successfully.");
+//	            } else {
+//	                System.out.println("HTML file does not exist: " + projectPath);
+//	            }
+//	        } catch (IOException e) {
+//	            System.out.println("An error occurred while opening the HTML file: " + e.getMessage());
+//	        }
+		 
+		 Thread.sleep(40000);
+		 
+	        String reportPath = System.getProperty("user.dir")+"/target/cucumber-reports123/index.html"; // Update with your report path
+	        Base.sendEmailWithReport(
+	                "jeevithan@agilecyber.com",
+	                "Cucumber Test Report",
+	                "Please find the attached test report.",
+	                reportPath
+	        );
+	    }
+	public static void main(String[] args) throws InterruptedException, AWTException, IOException {
+		sendReport();
+	}
+	
+}
